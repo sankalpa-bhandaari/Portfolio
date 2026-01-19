83d971636f2644d010be75d0310dd3108cd7f0ba
Sankalpa_Bhandaari <118331958+sankalpa-bhandaari@users.noreply.github.com>
2026-01-19 10:29:58 +0545

    Update

--- src/components/education.jsx
+++ src/components/education.jsx
@@ -8,7 +8,7 @@ const educationData = [
   },
   {
     degree: "Higher Secondary Education",
-    institution: "Rupa Secondary School",
+    institution: "Brahma Rupa Secondary School",
     year: "2017- 2022",
   },
 ];
